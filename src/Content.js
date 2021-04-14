6c29604cf5a472aa16261d59f0d99a69c7ba1128
Usama Taj <usama.taj@hotmail.com>
2021-04-14 17:50:07 +0500

    update Content component

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ const Content = () => {
   return (
       <Container>
         <Row>
-          <Col md="8" className="border border-dark vh-100">
+          <Col md="8" className="border border-dark h-100">
           <h1>Your Blogs</h1>
             <Card className="border rounded p-2 my-3">
               <Card.Title>
@@ -14,7 +14,7 @@ const Content = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Card.Text>
               <Card.Body className="p-2 m-1">
-                <Card.Link href="\#">Show Comments</Card.Link>
+                <Card.Link href="\">Show Comments</Card.Link>
               </Card.Body>
             </Card>
             
