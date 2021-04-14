02c5df1c7e25cc84f94b0341fc08844863dc9371
Usama Taj <usama.taj@hotmail.com>
2021-04-14 13:16:03 +0500

    change app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM form 'react-dom';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return ( 
     <h1>Welcome to My Blogging App</h1>
   );
 }
- 
 ReactDOM.render(<App/>, document.getElementById('root'));
